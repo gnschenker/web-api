de7c54ac29a7e8152bea264a621ee706616f5561
Gabriel N. Schenker <gnschenker@gmail.com>
2016-05-06 16:48:14 -0500

    Fixing Dockerfile and build process

--- web/Run-MsBuild.ps1
+++ web/Run-MsBuild.ps1
@@ -5,5 +5,5 @@ $ErrorActionPreference = "Stop"
 
 #run msbuild
 
-iex "& 'C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe' /p:VisualStudioVersion=12.0 /p:VSToolsPath=c:\windows\system32\msbuild.microsoft.visualstudio.web.targets.14.0.0\tools\vstoolspath"
+iex "& 'C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe web.csproj' /p:VisualStudioVersion=12.0 /p:VSToolsPath=c:\windows\system32\msbuild.microsoft.visualstudio.web.targets.14.0.0\tools\vstoolspath"
 if ($LASTEXITCODE -ne 0) {exit 1}
