810e8e525fd44b624cb729b026e93d859e2558f2
Gabriel N. Schenker <gnschenker@gmail.com>
2016-05-06 13:41:54 -0500

    first commit

---
+++ api/Controllers/ProjectsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace api.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Project> Get()
+        {
+            return new[]
+            {
+                new Project {id=1, name = "foo"},
+                new Project {id=2, name = "bar"},
+            };
+        } 
+    }
+
+    public class Project
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+#Introduction
+This repository is used to build a Windows Container running ASP.NET MVC 4.6
